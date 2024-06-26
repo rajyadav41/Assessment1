fee5690358750433db08e62663a2fbb4ce55e4cd
rajyadav41 <rajyadav4178@gmail.com>
2024-06-26 17:44:14 +0530

    Update Assessment1.js

--- Assessment1.js
+++ Assessment1.js
@@ -1,4 +1,7 @@
+// Variable Declaration
 let totalNFTs = 0;
+
+// Minting Function
 function mintNFT(name, description, imageUrl) {
     let newNFT = {
         name: name,
@@ -8,6 +11,8 @@ function mintNFT(name, description, imageUrl) {
     totalNFTs++;
     return newNFT;
 }
+
+// Listing Function
 function listNFTs(nftArray) {
     nftArray.forEach(function(nft) {
         console.log("Name: " + nft.name);
@@ -16,12 +21,18 @@ function listNFTs(nftArray) {
         console.log("-------------------------");
     });
 }
+
+// Fuction to get Total Supply
 function getTotalSupply() {
     console.log("Total number of NFTs: " + totalNFTs);
 }
+
+// Array Initialization and Minting NFTs
 let myNFTs = [];
 myNFTs.push(mintNFT("NFT 1", "Description of NFT 1", "https://rajyadav1x.com/nft1.jpg"));
 myNFTs.push(mintNFT("NFT 2", "Description of NFT 2", "https://rajyadav1x.com/nft2.jpg"));
 myNFTs.push(mintNFT("NFT 3", "Description of NFT 3", "https://rajyadav1x.com/nft3.jpg"));
+
+// Displaying NFTs and Total Supply
 listNFTs(myNFTs);
 getTotalSupply();
